3f53d337303241f7909e147022000a5b59c26d71
hilel941343 <hilel941343@gmail.com>
2025-05-15 16:53:46 +0300

    add allowed host

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y5)=qt_co0@fgo1q_9u0ry%z2you@ut(euz%5@@9ha#d=1f8up
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
